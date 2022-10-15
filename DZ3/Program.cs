1729b75c037216a33b6d92bea5646d113503d630
Nimere1990 <nimereiv@gmail.com>
2022-10-15 08:09:50 +0300

    Готовое ДЗ

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -8,3 +8,67 @@
 15 18
 */
 
+int linesVol = ReadInt("Введите количество строк: ");
+int columnsVol = ReadInt("Введите количество столбцов: ");
+int[,] array = new int[linesVol, columnsVol];
+int[,] secondArray = new int[linesVol, columnsVol];
+int[,] resultArray = new int[linesVol, columnsVol];
+
+FillArrayRandom(array);
+PrintArray2D(array);
+
+Console.WriteLine();
+
+FillArrayRandom(secondArray);
+PrintArray2D(secondArray);
+
+Console.WriteLine();
+
+if (array.GetLength(0) != secondArray.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        resultArray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultArray[i, j] += array[i, k] * secondArray[k, j];
+        }
+    }
+}
+PrintArray2D(resultArray);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandom(int[,] array)
+{
+    int[,] matrix = new int[linesVol, columnsVol];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
